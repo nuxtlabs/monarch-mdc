035d2910208310d52e25349b96dbd0b34ec6db53
Farnabaz <farnabaz@gmail.com>
2023-09-08 17:08:26 +0200

    fix: MDC code block highlighting

--- src/index.ts
+++ src/index.ts
@@ -1,244 +1,252 @@
 /**
- * Docus markdown language
+ * MDC language
  * Based on official markdown language
  */
-import type { languages } from 'monaco-editor-core';
+import type { languages } from 'monaco-editor-core'
 
 export const conf: languages.LanguageConfiguration = {
-	comments: {
-		blockComment: ['<!--', '-->']
-	},
-	brackets: [
-		['{', '}'],
-		['[', ']'],
-		['(', ')']
-	],
-	autoClosingPairs: [
-		{ open: '{', close: '}' },
-		{ open: '[', close: ']' },
-		{ open: '(', close: ')' },
-		{ open: '<', close: '>', notIn: ['string'] }
-	],
-	surroundingPairs: [
-		{ open: '(', close: ')' },
-		{ open: '[', close: ']' },
-		{ open: '`', close: '`' }
-	],
-	folding: {
-		markers: {
-			start: new RegExp('^\\s*<!--\\s*#?region\\b.*-->'),
-			end: new RegExp('^\\s*<!--\\s*#?endregion\\b.*-->')
-		}
-	}
-};
+  comments: {
+    blockComment: ['<!--', '-->']
+  },
+  brackets: [
+    ['{', '}'],
+    ['[', ']'],
+    ['(', ')']
+  ],
+  autoClosingPairs: [
+    { open: '{', close: '}' },
+    { open: '[', close: ']' },
+    { open: '(', close: ')' },
+    { open: '<', close: '>', notIn: ['string'] }
+  ],
+  surroundingPairs: [
+    { open: '(', close: ')' },
+    { open: '[', close: ']' },
+    { open: '`', close: '`' }
+  ],
+  folding: {
+    markers: {
+      start: new RegExp('^\\s*<!--\\s*#?region\\b.*-->'),
+      end: new RegExp('^\\s*<!--\\s*#?endregion\\b.*-->')
+    }
+  }
+}
 
 export const language = <languages.IMonarchLanguage>{
-	defaultToken: '',
-	tokenPostfix: '.md',
+  defaultToken: '',
+  tokenPostfix: '.md',
 
-	// escape codes
-	control: /[\\`*_\[\]{}()#+\-\.!]/,
-	noncontrol: /[^\\`*_\[\]{}()#+\-\.!]/,
-	escapes: /\\(?:@control)/,
+  // escape codes
+  control: /[\\`*_[\]{}()#+\-.!]/,
+  noncontrol: /[^\\`*_[\]{}()#+\-.!]/,
+  escapes: /\\(?:@control)/,
 
-	// escape codes for javascript/CSS strings
-	jsescapes: /\\(?:[btnfr\\"']|[0-7][0-7]?|[0-3][0-7]{2})/,
+  // escape codes for javascript/CSS strings
+  jsescapes: /\\(?:[btnfr\\"']|[0-7][0-7]?|[0-3][0-7]{2})/,
 
-	// non matched elements
-	empty: [
-		'area', 'base', 'basefont', 'br', 'col', 'frame',
-		'hr', 'img', 'input', 'isindex', 'link', 'meta', 'param'
-	],
+  // non matched elements
+  empty: [
+    'area', 'base', 'basefont', 'br', 'col', 'frame',
+    'hr', 'img', 'input', 'isindex', 'link', 'meta', 'param'
+  ],
 
-	tokenizer: {
-		root: [
-			[/^---$/, { token: '', next: '@frontmatter', nextEmbedded: 'yaml'}],
-			{ include: "markdown" }
-		],
+  tokenizer: {
+    root: [
+      [/^---$/, { token: '', next: '@frontmatter', nextEmbedded: 'yaml' }],
+      { include: 'markdown' }
+    ],
 
-		frontmatter: [
-			[/^\s*---\s*$/, { token: '', next: '@markdown', nextEmbedded: '@pop', bracket: '@close' }],
-			[/.*$/, 'variable.source']
-		],
+    frontmatter: [
+      [/^\s*---\s*$/, { token: '', next: '@markdown', nextEmbedded: '@pop', bracket: '@close' }],
+      [/.*$/, 'variable.source']
+    ],
 
-		markdown: [
+    markdown: [
 
-			// headers (with #)
-			[/^(\s{0,3})(#+)((?:[^\\#]|@escapes)+)((?:#+)?)/, ['white', 'keyword', 'keyword', 'keyword']],
+      // headers (with #)
+			[/^(\s{0,3}#+)/, { token: 'keyword', next: '@headercontent' }],
 
-			// headers (with =)
-			[/^\s*(=+|\-+)\s*$/, 'keyword'],
+      // headers (with =)
+      [/^\s*(=+|-+)\s*$/, 'keyword'],
 
-			// headers (with ***)
-			[/^\s*((\*[ ]?)+)\s*$/, 'meta.separator'],
+      // headers (with ***)
+      [/^\s*((\*[ ]?)+)\s*$/, 'meta.separator'],
 
-			// quote
-			[/^\s*>+/, 'comment'],
+      // quote
+      [/^\s*>+/, 'comment'],
 
-			// list (starting with * or number)
-			[/^\s*([\*\-+]|\d+\.)\s/, 'keyword'],
+      // list (starting with * or number)
+      [/^\s*([*\-+]|\d+\.)\s/, 'keyword'],
 
-			// code block (4 spaces indent)
-			[/^(\t|[ ]{4})[^ ].*$/, 'string'],
-
-
-
-			// code block (3 tilde)
-			[/^\s*~~~\s*((?:\w|[\/\-#])+)?\s*$/, { token: 'string', next: '@codeblock' }],
-
-			// github style code blocks (with backticks and language)
-			[/^\s*```\s*((?:\w|[\/\-#])+)\s*$/, { token: 'string', next: '@codeblockgh', nextEmbedded: '$1' }],
-
-			// github style code blocks (with backticks but no language)
-			[/^\s*```\s*$/, { token: 'string', next: '@codeblock' }],
-
-			// block components
-			[/^\s*(:{2,})([\w-]+)/, 'tag', '@componentWithData'],
-
-
-			// markup within lines
-			{ include: '@linecontent' },
+      // code block (4 spaces indent)
+      [/^(\t|[ ]{4})[^ ].*$/, 'string'],
 			
+      // code block (3 tilde)
+      [/^\s*~~~\s*((?:\w|[/\-#])+)?\s*$/, { token: '', next: '@codeblock' }],
 			
-		],
+      // github style code blocks (with backticks and language)
+      [/^\s*```\s*((?:\w|[/\-#])+)\s*$/, { token: '', next: '@codeblockgh', nextEmbedded: '$1' }],
+			
+      // github style code blocks (with backticks but no language)
+      [/^\s*```\s*$/, { token: '', next: '@codeblock' }],
+		
+      // MDC style code blocks (with backticks and language in brackets)
+      [/^\s*```\s*((?:\w|[/\-#])+)(\s*(\{[^}]*\}))?(\s*(\[[^\]]*\]))?(\s*(\{[^}]*\}))?.*$/, { token: '', next: '@codeblockmdc', nextEmbedded: '$1' }],
 
-		componentWithData: [
-			[/{/, 'tag', '@attributes'],
-			[/^\s*---\s*$/, { token: '', next: '@componentData', nextEmbedded: 'yaml'}],
-			[/^\s*::+\s*$/, 'tag', '@pop' ],
-			{include: '@component'}
-		],
+      // block components
+      [/^\s*(:{2,})([\w-]+)/, 'tag', '@componentWithData'],
 
-		component: [
-			// #slots
-			[/^\s*#[\w_-]*\s*$/, 'attribute.name.html'],
-			{ include: '@markdown'}
-		],
+      // markup within lines
+      { include: '@linecontent' }
 
-		componentData: [
-			[/^\s*---\s*$/, { token: '', next: '@pop', nextEmbedded: '@pop', bracket: '@close' }],
-			[/.*$/, 'variable.source']
-		],
+    ],
 
-		attributes: [
-			// class|id 
-			[/[^}=][^\s=}]*[\s.#]*/, 'attribute.name.html'],
-			[/[^}=][^\s=}]*(})/, ['attribute.name.html', {token: 'tag', next:'@pop'}]],
-			[/(=)("[^"]*"|[^"\s=]*)/, ['', 'string.html']],
-			[/}/, 'tag', '@pop']
-		],
+    componentWithData: [
+      [/{/, 'tag', '@attributes'],
+      [/^\s*---\s*$/, { token: '', next: '@componentData', nextEmbedded: 'yaml' }],
+      [/^\s*::+\s*$/, 'tag', '@pop'],
+      { include: '@component' }
+    ],
 
-		codeblock: [
-			[/^\s*~~~\s*$/, { token: 'string', next: '@pop' }],
-			[/^\s*```\s*$/, { token: 'string', next: '@pop' }],
-			[/.*$/, 'variable.source']
-		],
+    component: [
+      // #slots
+      [/^\s*#[\w_-]*\s*$/, 'attribute.name.html'],
+      { include: '@markdown' }
+    ],
 
-		// github style code blocks
-		codeblockgh: [
-			[/```\s*$/, { token: 'variable.source', next: '@pop', nextEmbedded: '@pop' }],
-			[/[^`]+/, 'variable.source'],
-		],
+    componentData: [
+      [/^\s*---\s*$/, { token: '', next: '@pop', nextEmbedded: '@pop', bracket: '@close' }],
+      [/.*$/, 'variable.source']
+    ],
 
-		linecontent: [
-			// escapes
-			[/&\w+;/, 'string.escape'],
-			[/@escapes/, 'escape'],
+    attributes: [
+      // class|id
+      [/[^}=][^\s=}]*[\s.#]*/, 'attribute.name.html'],
+      [/[^}=][^\s=}]*(})/, ['attribute.name.html', { token: 'tag', next: '@pop' }]],
+      [/(=)("[^"]*"|[^"\s=}]*)/, ['', 'string.html']],
+      [/}/, 'tag', '@pop']
+    ],
 
-			// various markup
-			// [/(\b__([\w]+)__)({)/, ['strong', '']],
-			[/\b__([^\\_]|@escapes|_(?!_))+__\b/, 'strong'],
-			[/\*\*([^\\*]|@escapes|\*(?!\*))+\*\*/, 'strong'],
-			[/\b_[^_]+_\b/, 'emphasis'],
-			[/\*([^\\*]|@escapes)+\*/, 'emphasis'],
-			[/`([^\\`]|@escapes)+`/, 'variable'],
+    codeblock: [
+      [/^\s*~~~\s*$/, { token: '', next: '@pop' }],
+      [/^\s*```\s*$/, { token: '', next: '@pop' }],
+      [/.*$/, 'variable.source']
+    ],
 
+    // github style code blocks
+    codeblockgh: [
+      [/```\s*$/, { token: '', next: '@pop', nextEmbedded: '@pop' }],
+      [/[^`]+/, 'variable.source']
+    ],
 
-			// links
-			[/^{+[^}]*\}+/, 'string.link'],
-			[/[^\*\_\)\]]\{+[^}]*\}+/, 'string.link'],
-			[/(!?\[)((?:[^\]\\]|@escapes)*)(\]\([^\)]+\))/, ['string.link', '', 'string.link']],
-			// [/(!?\[)((?:[^\]\\]|@escapes)*)(\])/, 'string.link'],
+    // github style code blocks
+    codeblockmdc: [
+      [/```\s*$/, { token: '', next: '@pop', nextEmbedded: '@pop' }],
+      [/[^`]+/, 'string']
+    ],
 
-			[/\{/, {token: 'tag', next: '@attributes'}],
+    headercontent: [
+      [/^/, 'keyword', '@pop'], // go back to markdown mode if we find a newline
+      [/((?:[^\\#]|@escapes)+)((?:#+)?)/, 'keyword'], // header text
+      { include: '@linecontent' }
+    ],
 
+    linecontent: [
+      // escapes
+      [/&\w+;/, 'string.escape'],
+      [/@escapes/, 'escape'],
 
-			// :block{#attribute}
-			[/(:)([\w-]+)({)/, ['tag', 'tag', {token: 'tag', next: '@attributes'}]],
-			// :block
-			[/(:)([\w-]+)/, ['tag', 'tag']],
+      // various markup
+      // [/(\b__([\w]+)__)({)/, ['strong', '']],
+      [/\b__([^\\_]|@escapes|_(?!_))+__\b/, 'strong'],
+      [/\*\*([^\\*]|@escapes|\*(?!\*))+\*\*/, 'strong'],
+      [/\b_[^_]+_\b/, 'emphasis'],
+      [/\*([^\\*]|@escapes)+\*/, 'emphasis'],
+      [/`([^\\`]|@escapes)+`/, 'variable'],
 
-			// [span]
-			[/(\[)([^\]]*)(\])({)/, ['string.link', '', 'string.link', {token: 'tag', next: '@attributes'}]],
-			[/(\[)([^\]]*)(\])/, ['string.link', '', 'string.link']],
+      // links
+      [/^{+[^}]*\}+/, 'string.link'],
+      [/[^*_)\]]\{+[^}]*\}+/, 'string.link'],
+      [/(!?\[)((?:[^\]\\]|@escapes)*)(\]\([^)]+\))/, ['string.link', '', 'string.link']],
+      // [/(!?\[)((?:[^\]\\]|@escapes)*)(\])/, 'string.link'],
 
-			// or html
-			{ include: 'html' },
-		],
+      [/\{/, { token: 'tag', next: '@attributes' }],
 
-		// Note: it is tempting to rather switch to the real HTML mode instead of building our own here
-		// but currently there is a limitation in Monarch that prevents us from doing it: The opening
-		// '<' would start the HTML mode, however there is no way to jump 1 character back to let the
-		// HTML mode also tokenize the opening angle bracket. Thus, even though we could jump to HTML,
-		// we cannot correctly tokenize it in that mode yet.
-		html: [
-			// html tags
-			[/<(\w+)\/>/, 'tag'],
-			[/<(\w+)/, {
-				cases: {
-					'@empty': { token: 'tag', next: '@tag.$1' },
-					'@default': { token: 'tag', next: '@tag.$1' }
-				}
-			}],
-			[/<\/(\w+)\s*>/, { token: 'tag' }],
+      // :block{#attribute}
+      [/(:)([\w-]+)({)/, ['tag', 'tag', { token: 'tag', next: '@attributes' }]],
+      // :block
+      [/(:)([\w-]+)/, ['tag', 'tag']],
 
-			[/<!--/, 'comment', '@comment']
-		],
+      // [span]
+      [/(\[)([^\]]*)(\])({)/, ['string.link', '', 'string.link', { token: 'tag', next: '@attributes' }]],
+      [/(\[)([^\]]*)(\])/, ['string.link', '', 'string.link']],
 
-		comment: [
-			[/[^<\-]+/, 'comment.content'],
-			[/-->/, 'comment', '@pop'],
-			[/<!--/, 'comment.content.invalid'],
-			[/[<\-]/, 'comment.content']
-		],
+      // or html
+      { include: 'html' }
+    ],
 
+    // Note: it is tempting to rather switch to the real HTML mode instead of building our own here
+    // but currently there is a limitation in Monarch that prevents us from doing it: The opening
+    // '<' would start the HTML mode, however there is no way to jump 1 character back to let the
+    // HTML mode also tokenize the opening angle bracket. Thus, even though we could jump to HTML,
+    // we cannot correctly tokenize it in that mode yet.
+    html: [
+      // html tags
+      [/<(\w+)\/>/, 'tag'],
+      [/<(\w+)/, {
+        cases: {
+          '@empty': { token: 'tag', next: '@tag.$1' },
+          '@default': { token: 'tag', next: '@tag.$1' }
+        }
+      }],
+      [/<\/(\w+)\s*>/, { token: 'tag' }],
 
-		// Almost full HTML tag matching, complete with embedded scripts & styles
-		tag: [
-			[/[ \t\r\n]+/, 'white'],
-			[/(type)(\s*=\s*)(")([^"]+)(")/, ['attribute.name.html', 'delimiter.html', 'string.html',
-				{ token: 'string.html', switchTo: '@tag.$S2.$4' },
-				'string.html']],
-			[/(type)(\s*=\s*)(')([^']+)(')/, ['attribute.name.html', 'delimiter.html', 'string.html',
-				{ token: 'string.html', switchTo: '@tag.$S2.$4' },
-				'string.html']],
-			[/(\w+)(\s*=\s*)("[^"]*"|'[^']*')/, ['attribute.name.html', 'delimiter.html', 'string.html']],
-			[/\w+/, 'attribute.name.html'],
-			[/\/>/, 'tag', '@pop'],
-			[/>/, {
-				cases: {
-					'$S2==style': { token: 'tag', switchTo: 'embeddedStyle', nextEmbedded: 'text/css' },
-					'$S2==script': {
-						cases: {
-							'$S3': { token: 'tag', switchTo: 'embeddedScript', nextEmbedded: '$S3' },
-							'@default': { token: 'tag', switchTo: 'embeddedScript', nextEmbedded: 'text/javascript' }
-						}
-					},
-					'@default': { token: 'tag', next: '@pop' }
-				}
-			}],
-		],
+      [/<!--/, 'comment', '@comment']
+    ],
 
-		embeddedStyle: [
-			[/[^<]+/, ''],
-			[/<\/style\s*>/, { token: '@rematch', next: '@pop', nextEmbedded: '@pop' }],
-			[/</, '']
-		],
+    comment: [
+      [/[^<-]+/, 'comment.content'],
+      [/-->/, 'comment', '@pop'],
+      [/<!--/, 'comment.content.invalid'],
+      [/[<-]/, 'comment.content']
+    ],
 
-		embeddedScript: [
-			[/[^<]+/, ''],
-			[/<\/script\s*>/, { token: '@rematch', next: '@pop', nextEmbedded: '@pop' }],
-			[/</, '']
-		],
-	}
-};
+    // Almost full HTML tag matching, complete with embedded scripts & styles
+    tag: [
+      [/[ \t\r\n]+/, 'white'],
+      [/(type)(\s*=\s*)(")([^"]+)(")/, ['attribute.name.html', 'delimiter.html', 'string.html',
+        { token: 'string.html', switchTo: '@tag.$S2.$4' },
+        'string.html']],
+      [/(type)(\s*=\s*)(')([^']+)(')/, ['attribute.name.html', 'delimiter.html', 'string.html',
+        { token: 'string.html', switchTo: '@tag.$S2.$4' },
+        'string.html']],
+      [/(\w+)(\s*=\s*)("[^"]*"|'[^']*')/, ['attribute.name.html', 'delimiter.html', 'string.html']],
+      [/\w+/, 'attribute.name.html'],
+      [/\/>/, 'tag', '@pop'],
+      [/>/, {
+        cases: {
+          '$S2==style': { token: 'tag', switchTo: 'embeddedStyle', nextEmbedded: 'text/css' },
+          '$S2==script': {
+            cases: {
+              $S3: { token: 'tag', switchTo: 'embeddedScript', nextEmbedded: '$S3' },
+              '@default': { token: 'tag', switchTo: 'embeddedScript', nextEmbedded: 'text/javascript' }
+            }
+          },
+          '@default': { token: 'tag', next: '@pop' }
+        }
+      }]
+    ],
+
+    embeddedStyle: [
+      [/[^<]+/, ''],
+      [/<\/style\s*>/, { token: '@rematch', next: '@pop', nextEmbedded: '@pop' }],
+      [/</, '']
+    ],
+
+    embeddedScript: [
+      [/[^<]+/, ''],
+      [/<\/script\s*>/, { token: '@rematch', next: '@pop', nextEmbedded: '@pop' }],
+      [/</, '']
+    ]
+  }
+}
